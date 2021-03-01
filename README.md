6cdbdd1fa7c8a95c5e06b6f64ff8a57b2e526e89
reneilpascua <rpascua2@my.bcit.ca>
2021-03-01 05:41:01 -0800

    small changes

--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 
 Problems from https://www.dailycodingproblem.com/, they send one to your email every day.
 
-Repo of my attempts to solve the daily problem. I don't have the premium version so I don't have the solutions. I'm not sure if they provide a set of test cases.
+Repo of my attempts to solve the daily problem, using Python 3.7.4. I don't have the premium version so I don't have the solutions. I'm not sure if they provide a set of test cases.
 
 ### Coming Soon
 A way to test, given many inputs and expected outputs in the template solution file.
--- helpers/helpers.py
+++ helpers/helpers.py
@@ -1,8 +1,21 @@
-# user input: a number list of known length
-def get_list_of_numbers():
-    n_ = int(input('How long is the list of numbers? '))
+# user input: an integer list of known length n
+def get_list_of_integers():
+    n_ = int(input('How long is the list of integers? '))
     list_ = []
     for i in range (0,n_):
-        num = int(input(f'enter the {i+1}\'th number in the list: '))
+        num = int(input(f'enter the {i+1}\'th integer in the list: '))
         list_.append(num)
     return [n_, list_]
+
+# user input: an integer list of known length, without specifying length
+def get_continuous_list_of_integers():
+    print('Enter a list of integers, and when you\'re finished, enter a non-integer.')
+    list_ = []
+    done = False
+    while not done:
+        try:
+            num = int(input('Enter an integer: '))
+            list_.append(num)
+        except: # general exception, namely from converting non-integer
+            done = True
+    return list_
