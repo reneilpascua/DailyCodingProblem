cc4d19ae289868cde2064b7d5cc6e1294cb1d2bb
reneilpascua <reneilpascuatm@gmail.com>
2021-04-19 09:23:57 -0700

    started on p54

--- problems/2021_04/p54_hard.py
+++ problems/2021_04/p54_hard.py
@@ -15,6 +15,73 @@ Implement an efficient sudoku solver.
 '''
 Implement solution
 '''
+class SudokuSolver():
+    '''
+    solves a sudoku using backtracking algo.
+
+    a more efficient solver is probably out of scope for an interview question lol
+    '''
+    def __init__(self):
+        self.initial = [] # the initial sudoku
+        self.soln = [] # solution to the sudoku
+
+    def set_unsolved_sudoku(self, grid):
+        '''
+        checks the input is a 9x9 list then sets initial sudoku.
+        for empty spaces, use a 0.
+        '''
+        if (len(grid) == 9) and (len(grid[0]) == 9):
+            self.initial = grid
+
+    def solve(self):
+        # try a number
+        # validate it
+        # if invalid, backtrack
+        return self.soln
+
+    
+    def __validate(self):
+        '''checks to see if the current state of the (partial) solution is valid'''
+        pass
+
+
+    def __validate_row(self, row_index):
+        '''returns false if a non-zero number in this row is repeated.'''
+        row = self.soln[row_index]
+
+        for i in range(9): # i= 0->8
+            if row.count(i+1) > 1:
+                return False
+        return True
+
+    def __validate_column(self, col_index):
+        '''returns false if a non-zero number in this column is repeated.'''
+        # get the column's values
+        column = []
+        for i in range(9):
+            column.append(self.soln[i][col_index])
+        
+        for i in range(9): # i= 0->8
+            if column.count(i+1) > 1:
+                return False
+        return True
+
+    def __validate_subgrid(self, sub_index):
+        '''returns false if a non-zero number in this subgrid is repeated.'''
+        # get the subgrid's values
+        subgrid = []
+
+    def __row_index(cell_index):
+        '''given the cell's index, what row is it in?'''
+        pass
+
+    def __col_index(cell_index):
+        '''given the cell's index, what column is it in?'''
+        pass
+
+    def __sub_index(cell_index):
+        '''given the cell's index, what subgrid is it in?'''
+        pass
 
 
 '''
