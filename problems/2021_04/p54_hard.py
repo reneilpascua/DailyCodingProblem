5053ea3fff4f768331f911e46d755e30be20386d
reneilpascua <reneilpascuatm@gmail.com>
2021-04-19 09:02:17 -0700

    added p54 this one is kinda ridiculous

---
+++ problems/2021_04/p54_hard.py
@@ -0,0 +1,24 @@
+import sys
+sys.path.append('../..')
+from helpers import helpers # only works if the script is in problems/month_folder/
+
+print('''
+[hard] apr. 19, 2021
+This problem was asked by Dropbox.
+
+Sudoku is a puzzle where you're given a partially-filled 9 by 9 grid with digits. 
+The objective is to fill the grid with the constraint that every row, column, and box (3 by 3 subgrid) must contain all of the digits from 1 to 9.
+
+Implement an efficient sudoku solver.
+''')
+
+'''
+Implement solution
+'''
+
+
+'''
+Driver
+'''
+if __name__ == '__main__':
+    pass
